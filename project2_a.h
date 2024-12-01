cd901cc5a494b5add83f6ba29f7126f171cf88af
kingnikoly <nikolaykraynev@yandex.ru>
2024-12-01 11:56:19 +0000

    Added cost_for_training_data, write_parameters_to_disk and read_parameters_from_disk functions

--- project2_a.h
+++ project2_a.h
@@ -9,6 +9,8 @@
 #include <cmath>
 #include <vector>
 #include <cassert>
+#include <fstream>
+#include <string>
 
 // Import LinearAlgebra namespace
 using namespace BasicDenseLinearAlgebra;
@@ -19,6 +21,11 @@ public:
     Layer(int input_size, int output_size, double l_rate);
     DoubleVector feed_forward(const DoubleVector &input) const;
     int get_input_dim() const { return input_dim; }
+    int get_output_dim() const { return input_dim; }
+    const DoubleMatrix& get_weights() const { return weights; }
+    const DoubleVector& get_bias() const { return bias; }
+    void set_weights(const DoubleMatrix& new_weights) { weights = new_weights; }
+    void set_bias(const DoubleVector& new_bias) { bias = new_bias; }
 
 private:
     DoubleMatrix weights;
@@ -41,18 +48,6 @@ Layer::Layer(int input_size, int output_size, double l_rate)
     weights = DoubleMatrix(output_size, input_size);
     bias = DoubleVector(output_size);
 }
-Layer::Layer(int input_size, int output_size, double l_rate)
-    : weights(output_size, input_size), bias(output_size) {
-    assert(input_size > 0);
-    assert(output_size > 0);
-
-    output_dim = output_size;
-    input_dim = input_size;
-    eta = l_rate;
-
-    weights = DoubleMatrix(output_size, input_size);
-    bias = DoubleVector(output_size);
-};
 
 DoubleVector Layer::feed_forward(const DoubleVector &input) const {
     assert(input.n() == input_dim);
@@ -81,6 +76,13 @@ class NeuralNetwork : public NeuralNetworkBasis{
     // Function to calculate the cost for given input and target output
     // Overrides base class function
     double cost(const DoubleVector& input, const DoubleVector& target_output) const override;
+
+    double cost_for_training_data(const std::vector<std::pair<DoubleVector,
+    DoubleVector>> training_data) const override;
+
+    void write_parameters_to_disk(const std::string& filename) const override;
+
+    void read_parameters_from_disk(const std::string& filename) override;
 
     private:
         std::vector<Layer> layers;
@@ -115,6 +117,86 @@ double NeuralNetwork::cost(const DoubleVector& input, const DoubleVector& target
     return cost / 2.0;
 }
 
+double NeuralNetwork::cost_for_training_data(const std::vector<std::pair<DoubleVector,
+    DoubleVector>> training_data) const {
+        double total_cost = 0;
+        
+        for (unsigned i = 0; i < training_data.size(); i++){
+            const DoubleVector& input = training_data[i].first;
+            const DoubleVector& target_output = training_data[i].second;
+            total_cost += cost(input, target_output);
+        }
 
+        return total_cost / training_data.size();
+    }
+
+// Definition of the write_parameters_to_disk function
+void NeuralNetwork::write_parameters_to_disk(const std::string& filename) const {
+    std::ofstream outfile(filename);
+    if (!outfile.is_open()) {
+        throw std::runtime_error("Unable to open file for writing");
+    }
+
+    for (const Layer& layer : layers) {
+        outfile << "TanhActivationFunction" << std::endl;
+        outfile << layer.get_input_dim() << " " << layer.get_output_dim() << std::endl;
+
+        const DoubleVector& bias = layer.get_bias();
+        for (unsigned i = 0; i < bias.n(); ++i) {
+            outfile << i << " " << bias[i] << std::endl;
+        }
+
+        const DoubleMatrix& weights = layer.get_weights();
+        for (unsigned i = 0; i < weights.n(); ++i) {
+            for (unsigned j = 0; j < weights.m(); ++j) {
+                outfile << i << " " << j << " " << weights(i, j) << std::endl;
+            }
+        }
+    }
+
+    outfile.close();
+}
+
+// Definition of the read_parameters_from_disk function
+void NeuralNetwork::read_parameters_from_disk(const std::string& filename) {
+    std::ifstream infile(filename);
+    if (!infile.is_open()) {
+        throw std::runtime_error("Unable to open file for reading");
+    }
+
+    layers.clear();
+
+    std::string activation_function;
+    while (infile >> activation_function) {
+        if (activation_function != "TanhActivationFunction") {
+            throw std::runtime_error("Unsupported activation function");
+        }
+
+        int input_dim, output_dim;
+        infile >> input_dim >> output_dim;
+
+        Layer layer(input_dim, output_dim, 0.0); // Learning rate is not needed here
+
+        DoubleVector bias(output_dim);
+        for (unsigned i = 0; i < output_dim; ++i) {
+            int index;
+            infile >> index >> bias[i];
+        }
+        layer.set_bias(bias);
+
+        DoubleMatrix weights(output_dim, input_dim);
+        for (unsigned i = 0; i < output_dim; ++i) {
+            for (unsigned j = 0; j < input_dim; ++j) {
+                int row, col;
+                infile >> row >> col >> weights(i, j);
+            }
+        }
+        layer.set_weights(weights);
+
+        layers.push_back(layer);
+    }
+
+    infile.close();
+}
 
 #endif // PROJECT2_A_H
